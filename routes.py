47f92db3ef258b4fe9f88dbe69d33f2d19ae7225
Jordan_Torres <jt.arontorres@gmail.com>
2021-03-20 23:03:01 -0700

    working on routes and adding api

--- routes.py
+++ routes.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
 import json
 import os
+import requests
 from dotenv import load_dotenv
 
 app = Flask(__name__)
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+API_URL = 'https://api.yelp.com/v3'
+SEARCH_PATH = '/v3/businesses/search'
+BUSINESS_PATH = '/v3/businesses/'
 
 
 @app.route('/')
@@ -24,6 +31,22 @@ def listing():
         return render_template('listing.html')
     except (ValueError, TypeError):
         return render_template('500.html'), 500
+
+def search(api_key, term, location):
+    """Query the Search API by a search term and location.
+    Args:
+        term (str): The search term passed to the API.
+        location (str): The search location passed to the API.
+    Returns:
+        dict: The JSON response from the request.
+    """
+
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'location': location.replace(' ', '+'),
+        'limit': SEARCH_LIMIT
+    }
+    return request(API_HOST, SEARCH_PATH, api_key, url_params=url_params)
 
 
 if __name__ == '__main__':
